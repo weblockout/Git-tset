a60e1e1a18a083ea4fae63751ac9e3be7613f601
Mastr <Mastr@KONUNG1>
2022-07-27 11:45:50 +0300

    added

--- Git-test-project/src/ru/il/test/Main.java
+++ Git-test-project/src/ru/il/test/Main.java
@@ -3,7 +3,7 @@ package ru.il.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!!");
 
 	}
 
